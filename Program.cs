0d2d6d8f7b7d75a41a59c824a1dc69c52078868a
IgorDmi <60873886+IgorDmi@users.noreply.github.com>
2021-02-25 13:52:26 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace get_set
+{
+    class Timetable
+    {
+        private string _request;
+
+        public string Request
+        {
+            get
+            {
+                return _request;
+            }
+            set
+            {
+                _request = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timetable work1 = new Timetable();
+
+            work1.Request = "cleaning";
+            Console.WriteLine(work1.Request);
+        }
+    }
+}
